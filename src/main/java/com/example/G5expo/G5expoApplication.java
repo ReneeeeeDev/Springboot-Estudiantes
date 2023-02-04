c8e558c653b5cb30b3ece9d0bebe0a8b3445b203
ReneeeeeDev <124427046+ReneeeeeDev@users.noreply.github.com>
2023-02-04 03:35:47 -0500

    Update G5expoApplication.java

--- src/main/java/com/example/G5expo/G5expoApplication.java
+++ src/main/java/com/example/G5expo/G5expoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Component
 public class G5expoApplication {
 
 	public static void main(String[] args) {
